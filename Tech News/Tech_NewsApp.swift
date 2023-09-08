d95f11b1c996250b9d0db2ce90067e7c585e84a7
yusuf <yusuferkoc99@gmail.com>
2023-09-08 15:21:31 +0300

    Initial Commit

---
+++ Tech News/Tech_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tech_NewsApp.swift
+//  Tech News
+//
+//  Created by Yusuf Erkoç on 8.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Tech_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
